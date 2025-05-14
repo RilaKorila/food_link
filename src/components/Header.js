8ba3629f91cc31d9038d5eed92c9b0d98976407e
moo-25 <>
2025-05-14 23:35:38 +0900

    headerの背景を変更

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import Image from "next/image"
 
 export default function Header() {
 return (
-    <div className="flex justify-between items-center px-4 py-3 border-b border-gray-100">
-        <Menu size={24} className="text-[#24262b]" />
+    <div className="flex justify-between items-center px-4 py-3 border-b bg-orange-400 border-gray-100">
+        <Menu size={24} className="text-black" />
         <h1 className="text-xl font-medium">Food Link</h1>
-        <div className="w-8 h-8 rounded-full overflow-hidden">
+        <div className="w-8 h-8 rounded-full overflow-hidden border border-black bg-white">
             <Image
                 src="/sampleProfile.png"
                 alt="ProfileImg"
